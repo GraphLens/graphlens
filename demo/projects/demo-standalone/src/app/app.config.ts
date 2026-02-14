5169c49953229ca790fcafbe2dd6ad78e768cdea
Dmitry Kostyuk <fosfor15.publisher@gmail.com>
2026-02-15 01:42:56 +0300

    feat(demo): Update apps and README instructions
    1. Refined integration of `angular-in-memory-web-api` library
    2. Updated README with instructions for using Cloud IDEs (GitHub Codespaces, Firebase Studio, etc.)

--- demo/projects/demo-standalone/src/app/app.config.ts
+++ demo/projects/demo-standalone/src/app/app.config.ts
@@ -1,16 +1,18 @@
 import {
     ApplicationConfig,
+    importProvidersFrom,
     provideBrowserGlobalErrorListeners,
     provideZonelessChangeDetection,
 } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { routes } from './app.routes';
 
 import { MessageService } from './services/message.service';
 import { InMemoryDataService } from './services/in-memory-data.service';
 import { HeroService } from './services/hero.service';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -18,8 +20,10 @@ export const appConfig: ApplicationConfig = {
         provideZonelessChangeDetection(),
         provideRouter(routes),
         provideHttpClient(),
+        importProvidersFrom(
+            HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 500 })
+        ),
         MessageService,
-        InMemoryDataService,
         HeroService
     ],
 };
--- demo/projects/demo-standalone/src/app/services/in-memory-data.service.ts
+++ demo/projects/demo-standalone/src/app/services/in-memory-data.service.ts
@@ -17,10 +17,13 @@ export class InMemoryDataService implements InMemoryDbService {
             { id: 19, name: 'Magma' },
             { id: 20, name: 'Tornado' }
         ];
+
         return { heroes };
     }
 
     genId(heroes: Array<Hero>): number {
-        return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+        return heroes.length > 0 ?
+            Math.max(...heroes.map(hero => hero.id)) + 1
+            : 11;
     }
 }
